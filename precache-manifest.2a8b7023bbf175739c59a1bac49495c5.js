9522c4ddd653a2187b3b69b104b3849cbd78e6a0
renzo01 <renzogomez577@gmail.com>
2019-12-10 15:24:44 -0500

    Updates

---
+++ precache-manifest.2a8b7023bbf175739c59a1bac49495c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "684cf3abc63618a873e8436496c63a34",
+    "url": "/monsterRolodex/index.html"
+  },
+  {
+    "revision": "26d36eec4aee5b833faa",
+    "url": "/monsterRolodex/static/css/main.7a7d18c5.chunk.css"
+  },
+  {
+    "revision": "8b41c4a390d39445ab63",
+    "url": "/monsterRolodex/static/js/2.17b76768.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsterRolodex/static/js/2.17b76768.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26d36eec4aee5b833faa",
+    "url": "/monsterRolodex/static/js/main.d7f90726.chunk.js"
+  },
+  {
+    "revision": "0810c64cd7981e807640",
+    "url": "/monsterRolodex/static/js/runtime-main.a363f914.js"
+  }
+]);
